517f06e38a4768704d43ce798eeef1dcf58904ad
m658761 <bob.warren@macys.com>
2016-04-09 17:03:55 -0700

    Table generation and source

--- scripts/app/freeform-designRequire.js
+++ scripts/app/freeform-designRequire.js
@@ -31,6 +31,7 @@ require(['augmented', 'augmentedPresentation'], function(Augmented, Presentation
     logger.info(APP_NAME + "Beginning Application...");
 
     var app = new Augmented.Presentation.Application("freeForm Designer");
+    app.registerStylesheet("https://fonts.googleapis.com/icon?family=Material+Icons");
     app.registerStylesheet("https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:400,300,400italic,100,700");
     app.start();
     logger.info(APP_NAME + "Starting Application...");
@@ -39,7 +40,23 @@ require(['augmented', 'augmentedPresentation'], function(Augmented, Presentation
 
     var ApplicationMediator = Augmented.Presentation.Mediator.extend({
         name:"mediator",
-        el: "#main"
+        el: "#main",
+        init: function() {
+            this.on("compile",
+                function(data) {
+                    if (data) {
+                        this.publish("viewer", "compile", data);
+                        this.publish("source", "compile", data);
+                    }
+                }
+            );
+            this.on("updateTestData", function(message) {
+                this.publish("viewer", "requestData", "requestData");
+            });
+            this.on("yourDataRequest", function(data) {
+                this.publish("source", "updateYourData", data);
+            });
+        }
     });
 
     var HeaderDecoratorView = Augmented.Presentation.DecoratorView.extend({
@@ -59,52 +76,122 @@ require(['augmented', 'augmentedPresentation'], function(Augmented, Presentation
             this.model = new Augmented.Model();
             this.syncModelChange("schema");
             this.syncModelChange("message");
+            this.setMessage("Ready.");
+        },
+        setMessage: function(message, bad) {
+            this.model.set("message", message);
+            if (bad) {
+                this.addClass("message", "bad");
+            } else {
+                this.removeClass("message", "bad");
+            }
+        },
+        getSchema: function() {
+            return this.model.get("schema");
         },
         validate: function(event) {
-            var schema = this.model.get("schema"), mess, s;
+            var schema = this.model.get("schema"), valid = false, data;
             try {
-                var data = JSON.parse(schema);
-                this.model.schema = data;
-                mess = this.boundElement("message");
-                mess.removeAttribute("class", "bad");
-                s = this.boundElement("schema");
-                s.removeAttribute("class", "bad");
-
-                this.model.set("message", "Schema is valid.");
-                //var mess = this.boundElement("message");
-                //Augmented.Presentation.Dom.setValue(mess, );
+                data = JSON.parse(schema);
+                this.removeClass("schema", "bad");
+                this.setMessage("Schema is valid.");
                 this.model.set("schema", Augmented.Utility.PrettyPrint(data));
-                //var schemaInput = this.boundElement("schema");
-                //Augmented.Presentation.Dom.setValue(schemaInput, Augmented.Utility.PrettyPrint(data));
+                valid = true;
             } catch(e) {
-                this.model.set("message", "Schema is not valid!  Could Not parse schema!");
-                mess = this.boundElement("message");
-                mess.setAttribute("class", "bad");
-                s = this.boundElement("schema");
-                s.setAttribute("class", "bad");
+                this.setMessage("Schema is not valid!  Could Not parse schema!", true);
+                this.addClass("schema", "bad");
             }
-
             logger.info(APP_NAME + "validating schema: " + schema);
+            return data;
         },
         clear: function(event) {
-            var mess, s;
-            // TODO: the unset is not either firing a change event or fails to set the value of the field
             this.model.unset("schema");
-            this.model.set("message", "Ready.");
-            mess = this.boundElement("message");
-            mess.removeAttribute("class", "bad");
-            s = this.boundElement("schema");
-            s.removeAttribute("class", "bad");
+            this.removeClass("schema", "bad");
+            this.setMessage("Ready.");
         },
         compile: function(event) {
+            var data = this.validate(), schema;
+            if (data) {
+                schema = this.model.get("schema");
+                this.sendMessage("compile", data);
+            }
             logger.info(APP_NAME + "compile button clicked! schema: " + this.model.get("schema"));
         }
     });
+
+    var MyTable = Augmented.Presentation.DirectDOMAutomaticTable.extend({
+    });
+
+    var myTableView = null;
 
     var ViewerDecoratorView = Augmented.Presentation.DecoratorView.extend({
         name: "viewer",
         el: "#viewer",
         init: function() {
+            this.on("compile", function(data) {
+                // do something
+                this.schema = data;
+                this.compile();
+                logger.debug(APP_NAME + "Viewer got the data");
+            });
+            this.on("requestData", function(message) {
+                this.sendMessage("yourDataRequest", this.data);
+                }
+            );
+        },
+        compile: function() {
+            if (myTableView && this.schema) {
+                myTableView.setSchema = this.schema;
+                myTableView.populate(this.data);
+                myTableView.render();
+            } else if (this.schema){
+                myTableView = new MyTable({
+                    schema: this.schema,
+                    data: this.data,
+                	el: "#renderWindow"
+                });
+                myTableView.render();
+            }
+            this.sendMessage("updateTestData", this.data);
+        },
+        generate: function() {
+            if (this.schema && this.schema.properties) {
+                var i = 0, ii = 0, keys = Object.keys(this.schema.properties), l = keys.length, obj = {};
+                this.data = [];
+                for (ii = 0; ii < 5; ii++) {
+                    obj = {};
+                    for (i = 0; i < l; i++) {
+                        obj[keys[i]] = this.makeUpData(
+                            this.schema.properties[keys[i]].type,
+                            this.schema.properties[keys[i]].format,
+                            this.schema.properties[keys[i]].enum
+                        );
+                    }
+                    this.data.push(obj);
+                }
+            }
+            this.compile();
+
+        },
+        makeUpData: function(type, format, en) {
+            if (type === "string") {
+                if (format && format === "email") {
+                    return "test" + Math.random() + "@example.com";
+                } else if (en) {
+                    var max = en.length, min = 0;
+                    var v = Math.floor(Math.random() * (max - min + 1)) + min;
+                    return en[v];
+                }
+                return "test" + Math.random();
+            } else if (type === "integer") {
+                return Math.floor(Math.random() * 100);
+            } else if (type === "number") {
+                return Math.random() * 100;
+            } else if (type === "boolean") {
+                var b = Math.floor(Math.random() * (1 - 0 + 1)) + 1;
+                return (b == 1) ? true : false;
+            }
+            return null;
         }
     });
 
@@ -113,6 +200,36 @@ require(['augmented', 'augmentedPresentation'], function(Augmented, Presentation
         el: "#source",
         init: function() {
             this.model = new Augmented.Model();
+            this.syncModelChange();
+            this.on("compile", function(data) {
+                this.compile(data);
+                logger.debug(APP_NAME + "Source got the data");
+            });
+            this.on("updateYourData", function(data) {
+                if (data) {
+                    this.model.set("data", Augmented.Utility.PrettyPrint(data));
+                }
+            });
+        },
+        compile: function(data) {
+            var javascript = "var schema = " + JSON.stringify(data) + ";\n\n" +
+                "var MyTable = Augmented.Presentation.DirectDOMAutomaticTable.extend({\n" +
+                "\tinit: function(options) { }\n" +
+                "});\n\n" +
+                "var at = new MyTable({ " +
+                    "\tschema: schema, \n" +
+                    "\tel: \"#autoTable\", \n" +
+                    "\tsortable: true,\n" +
+                    "\turl: \"http://www.example.com/data\"\n" +
+                "});\n\n" +
+                "at.render();";
+            this.model.set("javascript", javascript);
+
+            var html = "<div id=\"autoTable\" class=\"material\"></div>";
+            this.model.set("html", html);
+
+            var css = "<link type=\"text/css\" rel=\"stylesheet\" href=\"styles/table/material.css\"/>";
+            this.model.set("css", css);
         }
     });
 
