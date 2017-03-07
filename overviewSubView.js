6870948eb9ed30aacfe939c5ca92e5fe43649be8
thedocbwarren <jedi.master@sbcglobal.net>
2017-03-06 21:05:45 -0800

    Adding overview page, fixing breadcrumb navigation

--- overviewSubView.js
+++ overviewSubView.js
@@ -1,16 +1,20 @@
 const   Augmented = require("augmentedjs");
 	    Augmented.Presentation = require("augmentedjs-presentation");
-const   CONSTANTS = require("./constants.js");
+const   CONSTANTS = require("./constants.js"),
+		app = require("./application.js"),
+		overviewTemplate = require("./templates/overviewTemplate.js");
 
 //OverviewView
 module.exports = Augmented.Presentation.DecoratorView.extend({
     name: CONSTANTS.NAMES_AND_QUEUES.OVERVIEW,
     el: CONSTANTS.VIEW_MOUNT.OVERVIEW,
     init: function() {
+		this.render();
     },
     render: function() {
         var e = this.boundElement("overviewDetail");
         this.removeTemplate(e, true);
-        //this.injectTemplate("", e);
+        var ds = app.getDatastore();
+        this.injectTemplate(Handlebars.templates.overviewTemplate(ds.toJSON()), e);
     }
 });
